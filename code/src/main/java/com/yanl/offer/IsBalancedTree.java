dceb2d743f1d1ff654eaefce57fa64d64fa8bed8
liuyant <liuyant@zbj.com>
2020-08-10 11:16:10 +0800

    add IsBalancedTree

---
+++ code/src/main/java/com/yanl/offer/IsBalancedTree.java
@@ -0,0 +1,43 @@
+package com.yanl.offer;
+
+/**
+ * @author YanL
+ * @date 11:01 2020/8/10
+ * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+ *
+ */
+public class IsBalancedTree  {
+    private boolean isBalanced = true;
+    public boolean idBalancedTree(TreeNode root){
+        int depth = getDepth(root);
+        return isBalanced;
+    }
+
+    /**
+     * 获取左右子树深度并判断是否平衡
+     * @param root
+     * @return
+     */
+    public int getDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if(Math.abs(left - right) > 1){
+            isBalanced = false;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
