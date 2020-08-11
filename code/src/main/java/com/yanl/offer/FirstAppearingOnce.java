b1795d89cdae47f38eeb7215840682f2f07fb389
liuyant <liuyant@zbj.com>
2020-08-11 14:51:47 +0800

    add FirstAppearingOnce

---
+++ code/src/main/java/com/yanl/offer/FirstAppearingOnce.java
@@ -0,0 +1,37 @@
+package com.yanl.offer;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author YanL
+ * @date 14:39 2020/8/11
+ * 请实现一个函数用来找出字符流中第一个只出现一次的字符。
+ * 例如，当从字符流中只读出前两个字符"go"时，
+ * 第一个只出现一次的字符是"g"。当从该字符流中读出前六个字符“google"时，
+ * 第一个只出现一次的字符是"l"。
+ *
+ * 如果当前字符流没有存在出现一次的字符，返回#字符。
+ *
+ * 因为要返回第一个出现一次的字符
+ * 使用linkedHashMap保证插入顺序是有序的
+ */
+public class FirstAppearingOnce {
+    LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+    //Insert one char from stringstream
+    public void insert(char ch)
+    {
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+    }
+
+    //return the first appearence once char in current stringstream
+    public char firstAppearingOnce()
+    {
+        for(Character ch : map.keySet()){
+            if(map.get(ch) == 1){
+                return ch;
+            }
+        }
+        return '#';
+    }
+
+}
