0da93106c208ebfeb3b00794646bcc3f89696c69
Yanldt <imyanl.dt@gmail.com>
2020-08-12 19:53:08 +0800

    add TreeLinkNode

---
+++ code/src/main/java/com/yanl/offer/TreeLinkNode.java
@@ -0,0 +1,16 @@
+package com.yanl.offer;
+
+/**
+ * @author YanL
+ * @date 10:19 2020/8/12
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
