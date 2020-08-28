5a066f672ae52caaf2bfec6e884db826e18e1987
Yanldt <imyanl.dt@gmail.com>
2020-08-28 23:42:57 +0800

    add finally

--- code/src/main/java/com/yanl/Main.java
+++ code/src/main/java/com/yanl/Main.java
@@ -6,5 +6,16 @@ package com.yanl;
 public class Main {
     public static void main(String[] args) {
         System.out.println(6&1);
+        int a = testFinally();
+        System.out.println(a);
+    }
+
+    public static int testFinally(){
+        int i = 0;
+        try{
+            return i;
+        }finally {
+            i=i+1;
+        }
     }
 }
