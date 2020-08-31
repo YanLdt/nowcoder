db3542ef85d1a96c4f47c7435df52a634cfbb5d0
Yanldt <imyanl.dt@gmail.com>
2020-08-31 18:12:51 +0800

    add L0076MinimumWindowSubString

---
+++ code/src/main/java/com/yanl/leetcode/L0076MinimumWindowSubString.java
@@ -0,0 +1,20 @@
+package com.yanl.leetcode;
+
+/**
+ * @author YanL
+ * @date 17:08 2020/8/31
+ * 最小覆盖子串
+ * 给你一个字符串 S、一个字符串 T 。请你设计一种算法，可以在 O(n) 的时间复杂度内，
+ * 从字符串 S 里面找出：包含 T 所有字符的最小子串。
+ * 输入：S = "ADOBECODEBANC", T = "ABC"
+ * 输出："BANC"
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ */
+public class L0076MinimumWindowSubString {
+    public String minWindow(String s, String t) {
+        int lenS = s.length();
+        int lenT = t.length();
+        return "";
+    }
+}
