eb65fb21a4a739b30ec3546c47a7c5a2af4931f8
Yanldt <imyanl.dt@gmail.com>
2020-08-31 13:04:20 +0800

    add L0034FindFirstAndLastPositionElement

--- code/src/main/java/com/yanl/leetcode/L0034FindFirstAndLastPositionElement.java
+++ code/src/main/java/com/yanl/leetcode/L0034FindFirstAndLastPositionElement.java
@@ -7,6 +7,8 @@ package com.yanl.leetcode;
  * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
  * 你的算法时间复杂度必须是 O(log n) 级别。
  * 如果数组中不存在目标值，返回 [-1, -1]。
+ * 二分查找
+ * 找到目标元素就开始遍历左右两边
  */
 public class L0034FindFirstAndLastPositionElement {
     public int[] searchRange(int[] nums, int target) {
@@ -16,8 +18,26 @@ public class L0034FindFirstAndLastPositionElement {
         int[] res = new int[2];
         while (left <= right){
             int mid = left + (right - left) / 2;
-
+            if(nums[mid] == target){
+                int l = mid - 1;
+                int r = mid + 1;
+                while (l >= 0 && nums[l] == target){
+                    l--;
+                }
+                res[0] = l+1;
+                while (r < len && nums[r] == target){
+                    r++;
+                }
+                res[1] = r-1;
+                return res;
+            }
+            if(nums[mid] < target){
+                left = mid + 1;
+            }
+            if(nums[mid] > target){
+                right = mid - 1;
+            }
         }
-        return nums;
+        return new int[]{-1, -1};
     }
 }
