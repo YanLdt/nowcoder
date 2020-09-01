e302a5c94ee4d70939e3917aa67ba6063b7b6302
Yanldt <imyanl.dt@gmail.com>
2020-09-01 18:39:56 +0800

    add L0079WordSearch to be done

---
+++ code/src/main/java/com/yanl/leetcode/L0079WordSearch.java
@@ -0,0 +1,15 @@
+package com.yanl.leetcode;
+
+/**
+ * @author YanL
+ * @date 16:58 2020/9/1
+ * 单词搜索
+ * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用。
+ */
+public class L0079WordSearch {
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
